fcc9f53ded005374c5ccbc5210ad435f703e7ad6
Oceane Youyoutte <114257175+OceaneYYT@users.noreply.github.com>
2023-07-06 15:34:29 +0200

    Update api.py

--- api.py
+++ api.py
@@ -112,5 +112,5 @@ def shap_values():
             'shap_values_1': shap_val[1].tolist()}
 
 
-#if __name__ == '__main__':
-    #uvicorn.run(app, host='127.0.0.1', port=8000)
+if __name__ == '__main__':
+    uvicorn.run(api=app, host='127.0.0.1', port=8000)

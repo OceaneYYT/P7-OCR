b125ff49234201ab785f4a3c4663b3259614b9c3
Oceane Youyoutte <114257175+OceaneYYT@users.noreply.github.com>
2023-07-10 12:24:21 +0200

    Update test_dashboard.py

--- test_dashboard.py
+++ test_dashboard.py
@@ -2,9 +2,11 @@ from dashboard import get_prediction
 
 
 def test_get_prediction_accord():
+    '''Test la fonction get_prediction() du dashboard pour un client dont le prêt est considéré 'accordé'.'''
     assert get_prediction(192535) == (0.481, 'Accordé')
 
 
 def test_get_prediction_refus():
+    '''Test la fonction get_prediction() du dashboard pour un client dont le prêt est considéré 'refusé'.'''
     assert get_prediction(420554) == (0.678, 'Refusé')
 

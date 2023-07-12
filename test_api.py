94e19498c941aeebdd15a6364063c3a0ad998705
Océane Youyoutte <oceane.youyoutte@gmail.com>
2023-07-12 13:12:16 +0200

    Update test_api

--- test_api.py
+++ test_api.py
@@ -7,12 +7,14 @@ API_URL = "https://p7-ocr-fastapi-95768180a01f.herokuapp.com/"
 
 
 def test_welcome():
+    """Test la fonction welcome() de l'API."""
     response = requests.get(API_URL)
     assert response.status_code == status.HTTP_200_OK
     assert json.loads(response.content) == 'Welcome to the API'
 
 
 def test_check_client_id():
+    """Test la fonction check_client_id() de l'API avec un client faisant partie de la base de données."""
     url = API_URL + str(192535)
     response = requests.get(url)
     assert response.status_code == status.HTTP_200_OK
@@ -20,6 +22,7 @@ def test_check_client_id():
 
 
 def test_check_client_id_2():
+    """Test la fonction check_client_id() de l'API avec un client ne faisant pas partie de la base de données."""
     url = API_URL + str(100000)
     response = requests.get(url)
     assert response.status_code == status.HTTP_200_OK
@@ -27,6 +30,7 @@ def test_check_client_id_2():
 
 
 def test_get_prediction():
+    """Test la fonction get_prediction() de l'API."""
     url = API_URL + "prediction/" + str(192535)
     response = requests.get(url)
     assert response.status_code == status.HTTP_200_OK
